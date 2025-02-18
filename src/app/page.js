90ddc5b2d89d45c13f4bff43126ad7603e3d8808
aksho-sh <akshobhyasharma@yahoo.com>
2025-02-18 23:57:12 +0000

    some message was added

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home(){
       <h1>We are busy testing out ci/cd tools</h1>
       <p>We will change this and push to see if the pipeline is actually working:</p>
       <ul>
-        <li>The first component of the list, the project has</li>
+        <li>The first component of the list, the project has been modified here.</li>
       </ul>
     </div>
   );
